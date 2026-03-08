46a196bd573f0a814da6980594121c4d9cfcdae5
Gaute Lindkvist <lindkvis@gmail.com>
2026-03-08 06:47:51 +0100

    Poll server in Windows bat file and open browser when server responds

--- installer/windows/Start-PixlVault-Server.bat
+++ installer/windows/Start-PixlVault-Server.bat
@@ -4,5 +4,5 @@ set PIXLVAULT_PORT=9537
 
 start "" "%PIXLVAULT_APP_DIR%venv\Scripts\pixlvault-server.exe" %*
 
-timeout /t 4 /nobreak >nul
-start "" "http://localhost:%PIXLVAULT_PORT%"
+powershell -NoProfile -Command ^
+  "$url = 'http://localhost:%PIXLVAULT_PORT%/version'; $deadline = [DateTime]::UtcNow.AddSeconds(60); while ([DateTime]::UtcNow -lt $deadline) { try { Invoke-WebRequest -Uri $url -UseBasicParsing -TimeoutSec 2 -ErrorAction Stop | Out-Null; break } catch { Start-Sleep -Seconds 1 } }; Start-Process $url"
